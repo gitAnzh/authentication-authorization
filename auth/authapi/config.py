f49e16aadf9815571ccf70dee2d5e07a8982b20e
Mohsen Yousefi <mohsen.u3fi@hotmail.com>
2022-07-20 12:00:17 +0430

    change config

--- auth/authapi/config.py
+++ auth/authapi/config.py
@@ -5,7 +5,7 @@ import pymongo
 # database connection
 class db_connection:
     # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    myclient = pymongo.MongoClient('200.100.100.220', username='root', password='qweasdQWEASD')
+    myclient = pymongo.MongoClient('172.16.16.12', username='root', password='qweasdQWEASD')
     mydb = myclient["auth"]
     access_app_col = mydb["authapi_access"]
     user_col = mydb['authapi_user']
@@ -13,5 +13,5 @@ class db_connection:
     access_group = mydb['user_group']
     token_col = mydb['authtoken_token']
 
-    rabbit_connection = pika.URLParameters('amqp://rbtmq:DeVrab!t123@200.100.100.205:5672/%2F')
+    rabbit_connection = pika.URLParameters('amqp://rbtmq:DeVrab!t123@172.16.16.12:5672/%2F')
     queue_name = "auth"
